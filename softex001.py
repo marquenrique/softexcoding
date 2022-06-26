7fea0fb87225da92d7ee9ae160318e18a69fb4e9
Marcos Henrique <95144034+marquenrique@users.noreply.github.com>
2022-06-26 20:29:19 -0300

    Create softex001.py

---
+++ softex001.py
@@ -0,0 +1,22 @@
+nome_aluno = "João"
+
+nota1_aluno = 6
+
+nota2_aluno = 8
+
+media_aluno = nota1_aluno + nota2_aluno
+
+faltas_aluno = 2
+
+if(media_aluno < 7):
+    print(nome_aluno + "você foi reprovado.")
+
+
+if(faltas_aluno > 3):
+    print(nome_aluno + "você foi reprovado.")
+
+
+if(media_aluno >= 7):
+    print("Parabéns," + nome_aluno + "você foi aprovado!")
+
+
