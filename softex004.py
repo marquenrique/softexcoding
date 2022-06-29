1cb16bff2e94f826a9b32a8925d0a0fff947343c
Marcos Henrique <95144034+marquenrique@users.noreply.github.com>
2022-06-29 17:14:57 -0300

    commit

---
+++ softex004.py
@@ -0,0 +1,10 @@
+def calculadora(n1, n2, operacao):
+    if operacao == 'soma':
+       return n1 + n2
+    elif operacao == 'subtracao':
+       return n1 - n2 
+    elif operacao == 'multiplicacao':
+       return n1 * n2 
+    elif operacao == 'divisao':
+      return  n1 / n2 
+    else: return 0
