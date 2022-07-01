bd14046396f90050046c3906cac9133c8a5e6dac
Marcos Henrique <95144034+marquenrique@users.noreply.github.com>
2022-07-01 12:52:46 -0300

    commit

--- softex006.py
+++ softex006.py
@@ -1,3 +1,14 @@
-nome = input("digite seu nome")
-nome()
-print(f"seu nome é {nome}")
+nome = input("Digite seu nome completo")
+anoNascimento = int(input("Digite seu ano de nascimento"))
+
+def suaIdade():
+    try: 
+        if anoNascimento >= 1922 and anoNascimento <= 2021:
+            idadeAtual = 2022 - anoNascimento
+            print(f"{nome} você completou ou completará {idadeAtual} anos em 2022")
+    except Exception as erro:
+        print("Ocorreu um erro! Digite um ano válido")
+        while(erro == True):
+            nome 
+            anoNascimento
+            suaIdade()
